095abd41d258957ec82482ced3c07171ae4720e7
kanata <kanata10kan@gmail.com>
2024-08-18 14:54:16 +0100

    google -3

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.kanatajs.com', // ضع هنا رابط موقعك
+  generateRobotsTxt: true, // (اختياري) لتوليد ملف robots.txt
+  // إذا كان لديك صفحات ديناميكية، يمكنك استخدام الطرق المتقدمة للمسارات هنا
+};
---
+++ sitemap.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+function generateSiteMap() {
+  const baseUrl = 'https://www.kanatajs.com';
+  const pages = ['/', '/About', '/Projects', '/Contact', '/Register'];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  ${pages
+    .map(
+      page => `
+  <url>
+    <loc>${baseUrl}${page}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>${page === '/' ? '1.00' : '0.80'}</priority>
+  </url>`,
+    )
+    .join('')}
+</urlset>`;
+
+  fs.writeFileSync(
+    path.join(__dirname, 'public', 'sitemap.xml'),
+    sitemap,
+    'utf8',
+  );
+}
+
+generateSiteMap();
